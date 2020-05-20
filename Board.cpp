e1f1ecd92812675090ae39b5717acca0fa3afafe
maxkrakauer <maxkrakauer91@gmail.com>
2020-05-20 11:51:13 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -12,5 +12,24 @@ namespace WarGame{
 
 
 
+    Soldier*& Board::operator[](std::pair<int,int> location){
+
+    }
+    
+    
+    Soldier* Board::operator[](std::pair<int,int> location) const{
+
+    }
+    
+    
+    void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction){
+
+    }
+
+    
+    bool Board::has_soldiers(uint player_number) const{
+        
+    }
+
 
 }
